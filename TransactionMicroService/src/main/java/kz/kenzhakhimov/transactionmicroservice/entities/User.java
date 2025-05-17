98e486bf3ea058f5819efa584c896a9c6c6970b0
Sultanbek <skenzhahimov@gmail.com>
2025-05-17 19:39:19 +0500

    CSHS-3 - делаю с jpa таблицу

--- TransactionMicroService/src/main/java/kz/kenzhakhimov/transactionmicroservice/entities/User.java
+++ TransactionMicroService/src/main/java/kz/kenzhakhimov/transactionmicroservice/entities/User.java
@@ -1,15 +1,20 @@
 package kz.kenzhakhimov.transactionmicroservice.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name="users")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private int balance;
